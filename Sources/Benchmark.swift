348161355ed87209799b677d909fcaec901598b2
Aleksey Mashanov <a.mashanov@corp.mail.ru>
2017-02-22 11:50:43 +0300

    Ability to get numeric measurments for further usage

--- Sources/Benchmark.swift
+++ Sources/Benchmark.swift
@@ -1,9 +1,24 @@
 import Glibc
 
-let minCount = 4
 let minCpuTime = 0.4
 
-public func timethis(count: Int, title: String? = nil, _ body: () throws -> Void) rethrows {
+public struct Sample : CustomStringConvertible {
+	public let count: Int
+	let wcl: timespec
+	let usr: timeval
+	let sys: timeval
+
+	public var wallclock: Double { return Double(wcl) }
+	public var user: Double { return Double(usr) }
+	public var system: Double { return Double(sys) }
+	public var cpu: Double { return Double(usr + sys) }
+
+	public var description: String {
+		return "\(wcl) wallclock secs (\(usr) usr + \(sys) sys = \(usr + sys) CPU) @ \(Double(count) / cpu)/s (n=\(count))"
+	}
+}
+
+public func benchmark(count: Int = 1, _ body: () throws -> Void) rethrows -> Sample {
 	var clockStart = timespec()
 	var clockFinish = timespec()
 	var start = rusage()
@@ -18,8 +33,13 @@ public func timethis(count: Int, title: String? = nil, _ body: () throws -> Void
 	let wcl = clockFinish - clockStart
 	let usr = finish.ru_utime - start.ru_utime
 	let sys = finish.ru_stime - start.ru_stime
-	print("\(title != nil ? title! + ": " : "")\(wcl) wallclock secs (\(usr) usr + \(sys) sys = \(usr + sys) CPU)")
-	if count < minCount || Double(usr + sys) < minCpuTime {
+	return Sample(count: count, wcl: wcl, usr: usr, sys: sys)
+}
+
+public func timethis(count: Int, title: String? = nil, _ body: () throws -> Void) rethrows {
+	let sample = try benchmark(count: count, body)
+	print("\(title != nil ? title! + ": " : "")\(sample)")
+	if sample.cpu < minCpuTime {
 		print("\t(warning: too few iterations for a reliable count)")
 	}
 }
